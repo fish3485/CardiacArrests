9db13c7ae9d9fe814b062f6cf1d850a8d7a51610
B Fisher <benjamin.e.fisher@gmail.com>
2016-11-11 12:25:33 -0700

    Modifying column types

--- 1 - Load Data.R
+++ 1 - Load Data.R
@@ -2,6 +2,17 @@ library("readr")
 library("XML")
 
 arrest.data <- xmlParse("CPR summary report.xml") %>%
-  xmlToDataFrame()
+  xmlToDataFrame(colClasses = c(character, 
+                                date, 
+                                character, 
+                                numeric, 
+                                numeric, 
+                                numeric, 
+                                numeric, 
+                                numeric, 
+                                numeric, 
+                                integer,
+                                character)) %>%
+  as_tibble()
 
 write_csv(arrest.data, "Cardiac Arrest Analysis.csv", na = "NA", append = TRUE, col_names = TRUE)
