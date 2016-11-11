292503b5daddd9ac0d110817c844642ceb710972
B Fisher <benjamin.e.fisher@gmail.com>
2016-11-11 15:42:56 -0700

    Quick analysis of CPR data

--- 2 - Analysis.R
+++ 2 - Analysis.R
@@ -1,3 +1,14 @@
 library(ggplot2)
-library(dplyr)
+library(tidyverse)
 
+# Objects must be loaded from first script for analysis of compression ratio and rate
+
+qplot(rate, binwidth = 5)
+qplot(ratio, binwidth = 5)
+qplot(paused, binwidth = 30)
+qplot(shocks, binwidth = 1)
+
+summary(rate)
+summary(ratio)
+summary(paused)
+summary(shocks)
