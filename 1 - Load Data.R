c009e09f0190984f8dc4f98a4b30a304184d9d9d
B Fisher <benjamin.e.fisher@gmail.com>
2016-11-10 17:53:24 -0700

    Switched to pipes

--- 1 - Load Data.R
+++ 1 - Load Data.R
@@ -4,11 +4,16 @@ library("XML")
 
 devtools::session_info()
 
-arrest.data <- xmlParse("CPR summary report.xml")
-
-rootnode <- xmlRoot(arrest.data)
-
-arrest <- xmlToDataFrame("CPR summary report.xml") %>%
+arrest <- xmlParse("CPR summary report.xml") %>%
+  xmlRoot() %>%
+  xmlToDataFrame() %>%
   as_tibble()
+
+as.numeric(arrest$LongestPause)
+as.numeric(arrest$CompressionDepth)
+as.numeric(arrest$CompressionDepth)
+as.numeric(arrest$NumberOfShocks)
+
+summary(arrest$LongestPause)
 
 View(arrest)
